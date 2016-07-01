ab1bbcc7a38d7d5c9217d48f43ee0e88f22a8bc3
alexkoha <alexkoha@gmail.com>
2016-07-01 21:10:17 +0300

    Add files via upload

---
+++ Lab6/6.1/Accounts/AccountApplication/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using AccountsLib;
+
+namespace AccountApplication
+{
+    class Program
+    {
+        static void Main()
+        {
+            int choose;
+            int amount;
+
+            Account firstAccount = AccountFactory.CreateAccount(100);
+            Console.WriteLine("Choose what you want to do : \n (1) Deposit \n (2) Withdraw \n (3) Quary");
+            
+            bool convertedToInt = int.TryParse(Console.ReadLine(), out choose);
+
+            try
+            {
+                if (convertedToInt)
+                {
+                    switch (choose)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter amount to deposit : ");
+                            convertedToInt = int.TryParse(Console.ReadLine(), out amount);
+
+                            if (convertedToInt)
+                            {
+                                firstAccount.Deposit(amount);
+                                Console.WriteLine("Deposit finished successful!\n");
+                            }
+                            else Console.WriteLine("Somthing wrong...! try again.\n");
+                            break;
+
+                        case 2:
+                            Console.WriteLine("Enter amount to withdraw : ");
+                            convertedToInt = int.TryParse(Console.ReadLine(), out amount);
+
+                            if (convertedToInt)
+                            {
+                                Console.WriteLine(!firstAccount.Withdraw(amount)
+                                    ? "You will go into overdraft.this action aborted.\n"
+                                    : "Deposit finished successful!\n");
+                            }
+                            else Console.WriteLine("Somthing wrong...! try again.\n");
+                            break;
+
+                        case 3:
+                            Console.WriteLine($"Your balance is : {firstAccount.Balance()} \n");
+                            break;
+
+                        default:
+                            Console.WriteLine("You can choose just (1) (2) or (3).try again. \n ");
+                            break;
+                    }
+                }
+            }
+            catch (ArgumentOutOfRangeException )
+            {
+                Console.WriteLine("\nAmount can't be negativ.\nOperation aborted !\n");
+            }
+
+            Account secondAccount = AccountFactory.CreateAccount(300);
+
+            Console.WriteLine("Lets transfer money from your account. \nplease enter amount to transfer :");
+
+            convertedToInt = int.TryParse(Console.ReadLine(), out amount);
+
+
+            if (convertedToInt)
+            {
+                firstAccount.Transfer(ref secondAccount, amount);
+            }
+            else Console.WriteLine("Somthing wrong...! Notice : you can enter just numeric numbers.");
+
+        }
+    }
+}
---
+++ Lab6/6.1/Accounts/AccountsLib/Account.cs
@@ -0,0 +1,75 @@
+﻿
+using System;
+
+namespace AccountsLib
+{
+    public class Account
+    {
+        private readonly int _id;
+        private int _money;
+
+        internal Account(int id)
+        {
+            _id = id;
+            _money = 0;
+        }
+
+        public int Balance()
+        {
+            return _money;
+        }
+
+        public void Deposit(int addMoney)
+        {
+            if (addMoney<0) throw new ArgumentOutOfRangeException();
+            _money += addMoney;
+        }
+
+        public bool Withdraw(int withdrawMoney)
+        {
+            if (withdrawMoney < 0) throw new ArgumentOutOfRangeException();
+
+            if (_money >= withdrawMoney)
+            {
+                _money -= withdrawMoney;
+                return true;
+            }
+            //Console.WriteLine($"You will go into overdraft.Maximum thay you can Withdraw is {_money}");
+            return false;
+        }
+
+        public void Transfer(ref Account to , int amount)
+        {
+            try
+            {
+                Console.WriteLine("Before Transfer :" + Balance());
+                if (Withdraw(amount))
+                {
+                    to._money += amount;
+                }
+            }
+            finally
+            {
+                Console.WriteLine("Transfer attempt!");
+                Console.WriteLine("After Transfer :" + Balance());
+            }
+
+
+
+        }
+    }
+
+    public static class AccountFactory
+    {
+        public static int LastId;
+
+        public static Account CreateAccount(int initialBalance)
+        {
+            LastId++;
+            var newOneAccount = new Account(LastId);
+            newOneAccount.Deposit(initialBalance);
+
+            return newOneAccount;
+        }
+    }
+}

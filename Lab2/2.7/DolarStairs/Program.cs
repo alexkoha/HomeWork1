10c2ba7000fbe2bb3997e54caee3eacdde69272a
Moaid Hathot <moaid.hathot@outlook.com>
2016-07-18 13:53:12 +0300

    Lab 2.7

--- Lab2/2.7/DolarStairs/Program.cs
+++ Lab2/2.7/DolarStairs/Program.cs
@@ -12,6 +12,7 @@ namespace DolarStairs
             {
                 for (int i = 1; i <= n; ++i)
                 {
+                    //this is ok, but there are other ways to do so using a single line. Check the MSDN documentation for string.Format (also works using '$' string interpolation) and/or the constructors of the string class
                     for (int j = 0; j < i; ++j)
                     {
                         Console.Write("$");
@@ -19,6 +20,7 @@ namespace DolarStairs
                     Console.WriteLine();
                 }
             }
+            //The convention is C# is to write the else's body in a new line and to always use braces '{', '}'
             else Console.WriteLine("You entered something wrong ....");
         }
     }
